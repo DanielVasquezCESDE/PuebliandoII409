7826ac867862275a1fa1e1e50dea934bbb33745d
Your Name <you@example.com>
2023-07-04 20:43:06 -0500

    Se crea el menu de idiomas, sus strings en español, ingles y portugues. Se programa el inflador de menu y lo que pasa cuando se da en un item

--- app/src/main/java/com/example/pruebaiipuebliando409/Home.java
+++ app/src/main/java/com/example/pruebaiipuebliando409/Home.java
@@ -3,6 +3,8 @@ package com.example.pruebaiipuebliando409;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -32,5 +34,42 @@ public class Home extends AppCompatActivity {
             }
         });
 
+        botonRestaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Home.this, "Clic detectado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        botonTourism.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Home.this, "Clic detectado", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    //Para inflar o hacer aparecer menú
+
+    public boolean OnCreateOptionsMenu(Menu menujava){
+        getMenuInflater().inflate(R.menu.menuidiomas,menujava);
+        return true;
+    }
+
+     //Si el id es el id del item de idioma X entonces traducir a ese idioma:
+    public boolean onOptionsItemSelected(MenuItem itemsjava){
+        int itemSeleccionado = itemsjava.getItemId();
+        switch (itemSeleccionado) {
+            case(R.id.Op1Esp):
+                break;
+            case(R.id.Op2Eng):
+                break;
+            case(R.id.Op3Por):
+                break;
+            case(R.id.About):
+                break;
+        }
+        //CAda que detecta que se le da clic devuelve un verdadero
+        return super.onOptionsItemSelected(itemsjava);
     }
 }
